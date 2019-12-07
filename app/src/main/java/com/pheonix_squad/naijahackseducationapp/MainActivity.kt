8b094596d9176b3ea3cbc3b3b4828dbf308b03fe
dablaze-ufc <chukwuwauchenna@gmail.com>
2019-12-07 19:44:56 +0100

    Initial commit

---
+++ app/src/main/java/com/pheonix_squad/naijahackseducationapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pheonix_squad.naijahackseducationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
