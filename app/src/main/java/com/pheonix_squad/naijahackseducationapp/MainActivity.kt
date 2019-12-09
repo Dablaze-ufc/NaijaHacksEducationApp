1f3f6f6050eecd116d00863f86933426cd9b85c2
dablaze-ufc <chukwuwauchenna@gmail.com>
2019-12-09 21:03:51 +0100

    enabled youtube player API

--- app/src/main/java/com/pheonix_squad/naijahackseducationapp/MainActivity.kt
+++ app/src/main/java/com/pheonix_squad/naijahackseducationapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val APIKEY = "AIzaSyDH6Ct6DG1fmnTxnw9lSLDN_3QcRPHmjco"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
